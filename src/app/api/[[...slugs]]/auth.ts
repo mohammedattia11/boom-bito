b1f5163c31a77562482ffd8a0e0e8f9f7372f3a2
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-15 00:25:11 +0200

    feat:setup auth middleware

---
+++ src/app/api/[[...slugs]]/auth.ts
@@ -0,0 +1,34 @@
+import { redis } from "@/lib/redis";
+import Elysia from "elysia";
+
+class AuthError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "AuthError";
+  }
+}
+
+export const authMiddleware = new Elysia({ name: "auth" })
+  .error({ AuthError })
+  .onError(({ code, set }) => {
+    console.log(`Code: ${code}\nSet: ${set}`);
+    if (code === "AuthError") {
+      set.status = 401;
+      return { error: "unauthorized" };
+    }
+  })
+  .derive({ as: "scoped" }, async ({ query, cookie }) => {
+    console.log(`Query: ${query}\nCookie: ${cookie}`);
+    const roomId = query.roomId;
+    const token = cookie["x-auth-token"].value as string | undefined;
+    if (!roomId || !token) {
+      throw new AuthError(
+        "Missing roomID or user isn't allowrd to join the room"
+      );
+    }
+    const connected = await redis.hget<string[]>(`meta:${roomId}`, "connected");
+    if (!connected?.includes(token)) {
+      throw new AuthError("Current user is not a part of this room");
+    }
+    return { auth: { roomId, token, connected } };
+  });
