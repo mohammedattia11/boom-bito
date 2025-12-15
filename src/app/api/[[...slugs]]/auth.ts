d3f4f4cbb4919e948a8b3585013016fe46df7067
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-15 05:34:34 +0200

    fix:fixing auth middleware bug that prevents messages from being sent to the server

--- src/app/api/[[...slugs]]/auth.ts
+++ src/app/api/[[...slugs]]/auth.ts
@@ -11,21 +11,21 @@ class AuthError extends Error {
 export const authMiddleware = new Elysia({ name: "auth" })
   .error({ AuthError })
   .onError(({ code, set, error }) => {
-    console.log(`Code: ${code}\nSet: ${set}`);
     if (code === "AuthError") {
       set.status = 401;
       return { error: error.message };
     }
   })
   .derive({ as: "scoped" }, async ({ query, cookie }) => {
-    console.log(`Query: ${query}\nCookie: ${cookie}`);
     const roomId = query.roomId;
-    const token = cookie["x-auth-token"].value as string | undefined;
+    const token = cookie["x-auth-token"]?.value as string | undefined;
+
     if (!roomId || !token) {
       throw new AuthError(
         "Missing roomID or user isn't allowed to join the room"
       );
     }
+
     const connected = await redis.hget<string[]>(`meta:${roomId}`, "connected");
     if (!connected?.includes(token)) {
       throw new AuthError("Current user is not a part of this room");
