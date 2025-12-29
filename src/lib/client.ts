b2d4b0e8d4a546443d78377770c989e2fac552a8
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-29 21:45:13 +0200

    fix:updating edin file to access api

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,6 @@
 import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
 
-// this require .api to enter /api prefix
-export const client = treaty<app>('localhost:3000').api
+const baseURL = process.env.NEXT_PUBLIC_API_URL || (typeof window!==undefined ? window.location.origin : 'http://localhost:3000')
+
+export const client = treaty<app>(baseURL).api
