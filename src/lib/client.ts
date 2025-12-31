9fcd3530d4fe24ce3758aac658961c23563c5216
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-31 14:12:26 +0200

    fix:fix production cors issue

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,6 +1,7 @@
 import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
-
-const baseURL = process.env.NEXT_PUBLIC_API_URL || (typeof window!==undefined ? window.location.origin : 'http://localhost:3000')
-
-export const client = treaty<app>(baseURL).api
+const getBaseURL = () => {
+  if (process.env.NEXT_PUBLIC_API_URL) return `https://${process.env.NEXT_PUBLIC_API_URL}`;
+  return "http://localhost:3000";
+};
+export const client = treaty<app>(getBaseURL()).api
