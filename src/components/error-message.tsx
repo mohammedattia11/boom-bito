7ae07819911c256e3969d91aaf7d0318c90af94e
mohamed atia <mohamedd.attia11@gmail.com>
2025-12-31 22:07:35 +0200

    feat:adding close error message

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
 export function ErrorMessage({ errorMessage }: { errorMessage: string }) {
+  const [isOpened, setIsOpened] = useState(true);
   const message = {} as { header: string; content: string };
   if (errorMessage === "destroyed") {
     message.header = "ROOM DESTROYED";
@@ -13,9 +18,21 @@ export function ErrorMessage({ errorMessage }: { errorMessage: string }) {
     message.content = "This room at the maximum capacity.";
   }
   return (
-    <div className="bg-red-950/50 border border-red-900 p-4 text-center">
-      <p className="text-red-500 font-bold">{message.header}</p>
-      <p className="text-zinc-500 text-sm font-semibold mt-1">{message.content}</p>
-    </div>
+    isOpened && (
+      <div className=" relative bg-red-950/50 border border-red-900 p-4 text-center">
+        <p className="text-red-500 font-bold">{message.header}</p>
+        <p className="text-zinc-500 text-sm font-semibold mt-1">
+          {message.content}
+        </p>
+        <button
+          onClick={() => {
+            if (isOpened) setIsOpened(false);
+          }}
+          className="absolute right-3 top-0 cursor-pointer text-xl text-zinc-500"
+        >
+          &#10007;
+        </button>
+      </div>
+    )
   );
 }
